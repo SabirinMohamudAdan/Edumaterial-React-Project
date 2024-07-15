58a9fcd3fd9220f3f4729f7008576a5985917ded
SabirinMohamedAli <thaprinmohamett1333@gmail.com>
2024-07-15 16:06:38 +0300

    Updated the apps and product and other changes

--- website/web/src/App.js
+++ website/web/src/App.js
@@ -1,10 +1,11 @@
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, Link } from 'react-router-dom';
 import './App.css';
 
-import Backgroun from './deskmain/Home'; // Import the Home component
+import Backgroun from './deskmain/Home';
 import Nann from './deskmain/Nann';
-import ProductPage from './deskmain/ProductPage';
+
 import AboutPage from './deskmain/AboutPage';
 import Service from './deskmain/Service';
 import ContactPage from './deskmain/ContactPage';
@@ -12,7 +13,9 @@ import MoreAboutUs from './deskmain/MoreAboutUs';
 import ProductDetail from './deskmain/ProductDetail';
 import Customer from './deskmain/Customer';
 
-// import CartDetails from './deskmain/CartDetails';
+
+// import { ProductProvider } from './context/ProductContext';
+// import { CartProvider, useCartContext } from './context/CartContext';
 
 function App() {
   return (
@@ -89,7 +92,6 @@ const HomePage = () => (
     <Customer />
     <ProductList />
     <ContactPage />
-    {/* <footer write here /> */}
   </>
 );
 
